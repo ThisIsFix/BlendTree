7e74ba700252d85bb353ace407e43cd65d6e6736
ThisIsFix <50284031+ThisIsFix@users.noreply.github.com>
2020-02-06 17:09:51 -0500

    Add files via upload

---
+++ BlendTree.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendTree : MonoBehaviour
+{
+	public float smoothBlend = 0.1f;
+    private Animator anim;
+
+	void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+        
+    void Move(float x, float y)
+    {
+        anim.SetFloat("Blend", y, smoothBlend, Time.deltaTime);   	
+    }
+}
